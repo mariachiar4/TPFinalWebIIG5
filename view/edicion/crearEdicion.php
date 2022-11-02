f875b6d2eeb6c46ca799f947ab63607fada28a9f
nicolasarielmaldonado <nicolas.ariel.maldonado@gmail.com>
2022-11-02 12:37:39 -0300

    estilos crear edicion

--- view/edicion/crearEdicion.php
+++ view/edicion/crearEdicion.php
@@ -1,34 +1,38 @@
 {{> header}}
-<section>
-    <h2>Crear Edición</h2>
-    <form action="/edicion/procesarEdicion" method="post" enctype="application/x-www-form-urlencoded">
-        <div>
-            <label for="id_publicacion">Publicación</label>
-            <select name="id_publicacion" id="id_publicacion">
+<section class="form-container">
+    <h2 class="white-title">Crear Edición</h2>
+    <form class="container-flex" action="/edicion/procesarEdicion" method="post" enctype="application/x-www-form-urlencoded">
+        <div class="form-element">
+            <label class="form-label" for="id_publicacion">Publicación</label>
+            <select  name="id_publicacion" id="id_publicacion">
                 {{#publicaciones}} 
                     <option value={{id}}>{{nombre}}</option>
                 {{/publicaciones}} 
             </select>
         </div>
-        <div>
-            <label for="id_secciones">Secciones</label>
-            <div>
+        <div class="form-element">
+            <label class="form-label" for="id_secciones">Secciones</label>
+            <div class="two-column-grid">
                 {{#secciones}} 
-                    <input type="checkbox" name="id_secciones[]" id="id_secciones" value={{id}}>{{nombre}}</input>
+                <div>
+                    <input type="checkbox" name="id_secciones[]" id="id_secciones" value={{id}}>
+                    <span class="form-label">{{nombre}}</span>
+                </div>
                 {{/secciones}} 
             </div>
         </div>
         
-        <div>
-            <label for="nombre">Nombre</label>
+        <div class="form-element">
+            <label class="form-label" for="nombre">Nombre</label>
             <input type="text" name="nombre" id="nombre">
         </div>
         <!-- precio -->
-        <div>
-            <label for="precio">Precio</label>
+        <div class="form-element">
+            <label class="form-label" for="precio">Precio</label>
             <input type="number" name="precio" id="precio">
         </div>
-        <input type="submit" value="Crear Edición">
+
+        <input class="margin-top" type="submit" value="Crear Edición">
 
     </form>
 </section>
