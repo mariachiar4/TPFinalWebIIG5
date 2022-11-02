c6045da465b1ebb2ef4eaa931f018996fbd332e1
nicolasarielmaldonado <nicolas.ariel.maldonado@gmail.com>
2022-11-02 12:25:49 -0300

    estilos lista ediciones

--- view/edicion/accionesEdicion.php
+++ view/edicion/accionesEdicion.php
@@ -1,21 +1,32 @@
 {{> header}}
 <section>
+    <div class="white-title">
+        {{notificacion}}
+    </div>
 
-    <ul>
-        <ol>id_edicion nombre_Publicacion nombre_edicion fecha_creacion precio_edicion</ol>
+    <div class="grid-edicion">
+        <div class="white-title">ID</div>
+        <div class="white-title">Publicacion</div>
+        <div class="white-title">Nombre</div>
+        <div class="white-title">Fecha Creado</div>
+        <div class="white-title">Precio Edicion</div>
+        <div class="white-title"></div>
+        <!-- <ol>id_edicion nombre_Publicacion nombre_edicion fecha_creacion precio_edicion</ol> -->
     {{#ediciones}} 
-        <form action="/edicion/procesarAccionEdicion" method="post">
-            <input name="id" readonly="readonly" value='{{id}}'>
-            <input readonly="readonly" name="nombre_publicacion" type="text" value='{{nombre_publicacion}}'>
+        <form action="/edicion/procesarAccionEdicion" method="post" class="contents">
+            <input class="read-only" name="id" readonly="readonly" value='{{id}}'>
+            <input class="read-only" readonly="readonly" name="nombre_publicacion" type="text" value='{{nombre_publicacion}}'>
             <input name="nombre_edicion" type="text" value='{{nombre_edicion}}'>
-            <input readonly="readonly" value='{{fecha_creacion}}'>
+            <input class="read-only" readonly="readonly" value='{{fecha_creacion}}'>
             <input name="precio" type="number" value='{{precio}}'>
 
-
-            <input type="submit" name="accion" value='Editar'>
-            <input type="submit" name="accion" value='Eliminar'>
+            
+            <div class="w-full">
+                <input class="w-full" type="submit" name="accion" value='Editar'>
+                <input class="w-full" type="submit" name="accion" value='Eliminar'>
+            </div>
 
         </form>
     {{/ediciones}} 
-    </ul>
+    </div>
 </section>
--- view/panel.php
+++ view/panel.php
@@ -1,13 +1,13 @@
 {{> header}}
 <section class="container-flex">
-    <div class="container-flex ">
+    <div class="">
         <h3 class="titulo-admin-container" >Publicacion</h3>
         <a class="link-admin" href="/publicacion/altaPublicacion">Dar de alta Publicacion</a>
         <a class="link-admin" href="/publicacion/bajaPublicacion">Dar de baja Publicacion</a>
         <a class="link-admin" href="/publicacion/modificarPublicacion">Modificar Publicacion</a>
     </div>
 
-    <div class="container-flex margin-top">
+    <div class="margin-top">
         <h3 class="titulo-admin-container">Edicion</h3>
         <a class="link-admin" href="/edicion/crearEdicion">Crear Ediciones</a>
         <a class="link-admin" href="/edicion/accionesEdicion">Acciones de Ediciones</a>
