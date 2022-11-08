ad584e7be02c4fdc309838a4bb140da98e434c40
Rocio Monaco <rociomonaco@gmail.com>
2022-11-07 22:26:12 -0300

    articulo -> creacion articulo andando

--- model/ArticuloModel.php
+++ model/ArticuloModel.php
@@ -9,5 +9,12 @@ class ArticuloModel
         $this->database = $database;
     }
 
+    public function crearArticulo($id_edicion_seccion, $id_usuario_creador, $id_estado, $lat, $lon, $titulo, $bajada, $foto, $contenido){
+        return $this->database->execute("INSERT INTO articulo VALUES (null, $id_estado, $id_edicion_seccion, $id_usuario_creador, $lat, $lon, '$titulo', '$bajada', '$foto', '$contenido')");
+    }
+    public function getLastPublicacion(){
+        return $this->database->query("SELECT * FROM articulo WHERE id = (SELECT MAX(id) FROM articulo)");
+
+    }
 
 }
