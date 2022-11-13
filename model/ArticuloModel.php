af7438483672316f56a150ce457e59fe6f418ffc
nicolasarielmaldonado <nicolas.ariel.maldonado@gmail.com>
2022-11-13 19:29:18 -0300

    fix obtenerArticulosSegunPublicacion

--- model/ArticuloModel.php
+++ model/ArticuloModel.php
@@ -31,22 +31,19 @@ class ArticuloModel
     public function getArticulo($id){
         return $this->database->query("SELECT a.id, u.nombre as usuarioCreador, s.nombre as seccion, a.titulo, a.bajada, a.fotos, a.contenido, a.lat, a.lon 
                                 FROM articulo a 
-                                JOIN usuario u on  u.id = a.id_usuarioCreador
-                                JOIN edicion_seccion ed on  a.id_edicionSeccion = ed.id 
-                                JOIN seccion s on s.id = ed.id_seccion
+                                LEFT JOIN usuario u on  u.id = a.id_usuarioCreador
+                                LEFT JOIN edicion_seccion ed on  a.id_edicionSeccion = ed.id 
+                                LEFT JOIN seccion s on s.id = ed.id_seccion
                                 WHERE a.id = $id");
     }
 
     public function obtenerArticulosSegunPublicacion($id_publicacion){
-        return $this->database->query("SELECT a.id, a.titulo, a.bajada, a.fotos FROM articulo a
-                                            JOIN edicion_seccion ed ON ed.id = a.id_edicionSeccion  
-                                            WHERE ed.id_edicion = 
-                                                (SELECT e1.id FROM edicion e1  
-                                                    WHERE e1.fecha_creacion = (
-                                                        SELECT MAX(e2.fecha_creacion) FROM edicion e2 
-                                                        WHERE id_publicacion = $id_publicacion)
-                                                )
-                                            AND a.id_estado = 1");
+        return $this->database->query("SELECT a.id, a.titulo, a.bajada, a.fotos 
+                                FROM articulo a
+                                LEFT JOIN edicion_seccion ed ON ed.id = a.id_edicionSeccion
+                                LEFT JOIN edicion e ON e.id = ed.id_edicion
+                                WHERE e.id_publicacion = $id_publicacion
+                                AND id_estado = 3");
                                             // cuando este terminado cambiar a 3
     }
 }
